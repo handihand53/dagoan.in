276763db11d54280afd38ec48332291dabf8b94d
Handi <handihand53@gmail.com>
2020-09-13 16:54:09 +0700

    first init project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,10 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-card
+    dark="true">
+      punten
+    </v-card>
   </div>
 </template>
 
